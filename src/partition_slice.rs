f5ca90ff7f40a68cd2f607315eea91e27c4d0c7a
Joseph Ensminger <joetensminger@gmail.com>
2023-07-10 16:14:46 -0500

    tested & added documentation to partition_in_place

--- src/partition_slice.rs
+++ src/partition_slice.rs
@@ -1,4 +1,11 @@
-use std::cmp::PartialOrd;
+/* 
+What is an Array Partition? 
+Reordering a list by splitting it into two parts around a pivot element based on a condition/comparison:
+        (1) a lower half of items matching
+        (2) an upper half not matching
+*/
+
+use std::cmp::PartialOrd; /* items in list must have a Partial Order since this a comparison-based algorithm */
 pub fn partition_in_place<T: PartialOrd + Copy> (slice: &mut [T], pivot_index: usize) -> usize {
 	let pivot = slice[pivot_index];
 	slice.swap(0, pivot_index);
@@ -27,14 +34,11 @@ mod tests {
 		assert_eq!(pivot_position, 1);
         }
 
-        fn it_works_2(){
-        // From a string literal
-        let s = String::from("Hello, world!");
-        // Using the `to_string` method
-        let s = "Hello, world!".to_string();
-        // From a formatted string
-        let s = format!("Hello, {}", "world!");
-        // From a character iterator
-        let s: String = "Hello, world!".chars().collect();
+        #[test]
+        fn it_works_2() {
+                let mut slice = vec!["E", "D", "C", "B", "A"];
+		let pivot_position = partition_in_place(&mut slice, 3); /* pivot = "B" */ 
+                assert_eq!(vec!["A", "B", "C", "E", "D"], slice); /* all items to the left of the pivot are in partial order, not total order */
+		assert_eq!(pivot_position, 1);
         }
 }
